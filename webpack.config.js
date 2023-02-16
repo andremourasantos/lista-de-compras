1ee4a16241c5ca8949c283ca4eb23826a3d23eaf
André Moura <contato@andremourasantos.com>
2023-02-16 19:51:55 -0300

    Versão de teste 2.1 - Correção de erro que fazia com que a página recarregasse infinitamente. Por algum motivo, agora é necessário indicar o caminho /app/arquivo para o js e css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,45 @@
-//DESENVOLVIMENTO
 const path = require('path')
 
-module.exports = [
-    {
-        mode: "development",
-        entry: "./src/app-index.js",
-        output: {
-            path: path.resolve(__dirname, "public"),
-            filename: "app.js"
-        },
-        watch: true,
-        devtool: 'eval-source-map'
-    },
-    {
-        mode: "development",
-        entry: "./src/app.js",
-        output: {
-            path: path.resolve(__dirname, "public/app"),
-            filename: "app.js"
-        },
-        watch: true,
-        devtool: "eval-source-map"
-    }
-]
-
-//PRODUÇÃO
-// const path = require('path')
-
+//DESENVOLVIMENTO
 // module.exports = [
 //     {
-//         mode: "production",
+//         mode: "development",
 //         entry: "./src/app-index.js",
 //         output: {
 //             path: path.resolve(__dirname, "public"),
 //             filename: "app.js"
-//         }
+//         },
+//         watch: true,
+//         devtool: 'eval-source-map'
 //     },
 //     {
-//         mode: "production",
+//         mode: "development",
 //         entry: "./src/app.js",
 //         output: {
 //             path: path.resolve(__dirname, "public/app"),
 //             filename: "app.js"
-//         }
+//         },
+//         watch: true,
+//         devtool: "eval-source-map"
 //     }
 // ]
+
+//PRODUÇÃO
+module.exports = [
+    {
+        mode: "production",
+        entry: "./src/app-index.js",
+        output: {
+            path: path.resolve(__dirname, "public"),
+            filename: "app.js"
+        }
+    },
+    {
+        mode: "production",
+        entry: "./src/app.js",
+        output: {
+            path: path.resolve(__dirname, "public/app"),
+            filename: "app.js"
+        }
+    }
+]
