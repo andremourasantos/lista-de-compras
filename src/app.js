cecb597ec96d7a7e78bf76751a63d2f778e4a731
André Moura <contato@andremourasantos.com>
2023-04-06 18:24:11 -0300

    Adicionada função resgatarItensDaLista e DATA_ITEM_ADICIONAR OK, outras funções do DATA_ITEM não operacionais

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, connectFirestoreEmulator, getDoc, doc, query, collection, orderBy, setDoc, serverTimestamp } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator, getDoc, doc, query, collection, orderBy, setDoc, serverTimestamp, onSnapshot } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAAdgbV_X6cdkZWytuuUuA2-_XlvL_V4mo",
@@ -14,20 +14,23 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const BANCO_DE_DADOS = getFirestore(app);
-(() => {if(window.location.hostname === 'localhost'){
-    connectFirestoreEmulator(BANCO_DE_DADOS, 'localhost', 8080);
-    alert('conectando banco de dados local.')
-}})()
+
+(() => {
+    if(window.location.hostname === 'localhost'){
+        connectFirestoreEmulator(BANCO_DE_DADOS, 'localhost', 8080);
+    }
+})();
 //↑↑ CONFIGURAÇÕES DO FIREBASE
 
+//↓↓ CHECAGEM INICIAL
 const USUARIO = {
     UID: '',
     NOME: '',
     EMAIL: '',
-    DOC_REFERENCIA: ''
+    DOC_REFERENCIA: '',
+    LISTA_DE_COMPRAS: ''
 };
 
-//↓↓ CHECAGEM INICIAL
 onAuthStateChanged(auth, async (usuario) => {
     if(usuario){
         document.querySelector('header img').src = usuario.photoURL;
@@ -36,29 +39,115 @@ onAuthStateChanged(auth, async (usuario) => {
         USUARIO.UID = usuario.uid;
         USUARIO.NOME = usuario.displayName;
         USUARIO.EMAIL = usuario.email;
-        USUARIO.DOC_REFERENCIA = doc(BANCO_DE_DADOS, 'lista-de-compras', USUARIO.UID);
-
-        checaBancoDeDadosPorListaDoUsuario();
+        USUARIO.DOC_REFERENCIA = doc(BANCO_DE_DADOS, 'lista-de-compras', USUARIO.UID)
+        USUARIO.LISTA_DE_COMPRAS = collection(BANCO_DE_DADOS, 'lista-de-compras', USUARIO.UID, 'lista')
+        checarBancoDeDadosPorListaDoUsuario();
     } else {
         window.open('/', '_self')
     }
 });
 
-async function checaBancoDeDadosPorListaDoUsuario () {
+async function checarBancoDeDadosPorListaDoUsuario () {
     if((await getDoc(USUARIO.DOC_REFERENCIA)).exists()){
-        alert('banco de dados do usuário existe.')
+        resgatarItensDaLista();
     } else {
-        alert('banco de dados do usuário não existe')
-
         setDoc(USUARIO.DOC_REFERENCIA, {
             nome: USUARIO.NOME,
             email: USUARIO.EMAIL,
             criacao: serverTimestamp()
-        })
-
-        alert('banco de dados do usuário criado.')
+        });
     }
 }
+
+async function resgatarItensDaLista () {
+    const CONSULTA = query(USUARIO.LISTA_DE_COMPRAS, orderBy('criacao', 'asc'))
+
+    onSnapshot(CONSULTA, (snapshot) => {
+        let itens_da_lista = []
+
+        snapshot.forEach(documento => {
+            itens_da_lista.push({...documento.data(), id: documento})
+        })
+
+        if(itens_da_lista.length == 0){return}
+
+        itens_da_lista.forEach(documento => {
+            DATA_ITEM.ADICIONAR(documento)
+        })
+    })
+}
+
+//↓↓ AÇÕES GERAIS
+const DATA_ITEM = {
+    OBTER_ELEMENTO_PAI: (evento) => {
+        let pai = evento.currentTarget;
+        while (pai.nodeName != 'ARTICLE'){
+            pai = pai.parentNode;
+        };
+        return pai;
+    },
+    OBTER_ID: (elemento_pai) => {
+        return elemento_pai.getAttribute('data-item-id');
+    },
+    SALVAR: () => {
+        /*ADICIONA O ITEM À NUVEM*/
+        
+    },
+    ADICIONAR: (item_info) => {
+        /*ADICIONA O ITEM AO DOM*/
+        const modelo = document.importNode(document.querySelector('template').content, true)
+        const item = document.importNode(modelo, true)
+
+        //Adicionando informações
+        item.querySelector('p').textContent = item_info.nome
+
+        const container = document.createElement('article')
+        container.classList.add('item_da_lista')
+        container.setAttribute('data-item-id', item_info.id)
+
+        container.appendChild(item)
+
+        //Adicionando interações
+        container.querySelector('section').addEventListener('click', (e)=>{
+            e.stopImmediatePropagation()
+        })
+
+        container.addEventListener('click', () => {
+            container.classList.toggle('editar_item')
+        })
+
+        DATA_ITEM_ACAO.EDITAR(container)
+        DATA_ITEM_ACAO.DELETAR(container)
+
+        //Anexando ao DOM
+        document.querySelector('main').appendChild(container)
+    },
+    EDITAR: (item_id, item_info) => {
+        document.querySelector(`[data-item-id="${item_id}"] p`).textContent = item_info.nome
+    },
+    REMOVER: (item_id) => {
+        document.querySelector(`[data-item-id="${item_id}"]`).style.animationName = 'deletar_item'
+
+        setTimeout(() => {
+            document.querySelector(`[data-item-id="${item_id}"]`).remove()
+        }, 150);
+    },
+    NOTIFICAR: (nome_icone, texto) => {        
+        document.querySelector('#notificacao .material-symbols-rounded').textContent = nome_icone
+        document.querySelector('#notificacao p').textContent = texto
+
+        document.querySelector('#notificacao').style.display = 'grid'
+
+        if (DATA_ITEM.timeoutId) {
+            console.log(1)
+            clearTimeout(DATA_ITEM.timeoutId)
+        } else {console.log(2)}
+
+        DATA_ITEM.timeoutId = setTimeout(() => {
+            document.querySelector('#notificacao').style.display = 'none'
+        }, 3000)
+    }
+};
 
 //↓↓ BOTÕES DO MENU
 (() => {
