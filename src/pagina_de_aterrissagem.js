b0b54bf317b4073f7d631d776901fb90bc9aa43d
André Moura <contato@andremourasantos.com>
2023-04-06 13:09:59 -0300

    script da página de aterrissagem concluído

---
+++ src/pagina_de_aterrissagem.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAdgbV_X6cdkZWytuuUuA2-_XlvL_V4mo",
+    authDomain: "groceries-app-451f5.firebaseapp.com",
+    projectId: "groceries-app-451f5",
+    storageBucket: "groceries-app-451f5.appspot.com",
+    messagingSenderId: "1010463074472",
+    appId: "1:1010463074472:web:7a51918a58c895cfaf7a9d"
+};
+
+const app = initializeApp(firebaseConfig);
+const AUTENTICACAO_GOOGLE = new GoogleAuthProvider();
+const auth = getAuth();
+//↑↑ CONFIGURAÇÕES DO FIREBASE
+
+//↓↓ LOGIN
+(()=>{
+    document.querySelector('[data-btn-login="google"]').addEventListener('click', () => {
+        signInWithPopup(auth, AUTENTICACAO_GOOGLE)
+            .then(()=>{return})
+            .catch(()=>{
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                alert(errorMessage)
+            })
+    })
+})()
+
+//↓↓ APÓS LOGIN
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const uid = user.uid;
+        window.open('/app', '_self')
+    } else {return}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path')
 
 //DESENVOLVIMENTO
-// module.exports = [
-//     {
-//         mode: "development",
-//         entry: "./src/app-index.js",
-//         output: {
-//             path: path.resolve(__dirname, "public"),
-//             filename: "app.js"
-//         },
-//         watch: true,
-//         devtool: 'eval-source-map'
-//     },
-//     {
-//         mode: "development",
-//         entry: "./src/app.js",
-//         output: {
-//             path: path.resolve(__dirname, "public/app"),
-//             filename: "app.js"
-//         },
-//         watch: true,
-//         devtool: "eval-source-map"
-//     }
-// ]
-
-//PRODUÇÃO
 module.exports = [
     {
-        mode: "production",
-        entry: "./src/app-index.js",
+        mode: "development",
+        entry: "./src/pagina_de_aterrissagem.js",
         output: {
             path: path.resolve(__dirname, "public"),
-            filename: "app.js"
-        }
+            filename: "index.js"
+        },
+        watch: true,
+        devtool: 'eval-source-map'
     },
     {
-        mode: "production",
+        mode: "development",
         entry: "./src/app.js",
         output: {
             path: path.resolve(__dirname, "public/app"),
             filename: "app.js"
-        }
+        },
+        watch: true,
+        devtool: "eval-source-map"
     }
 ]
+
+//PRODUÇÃO
+// module.exports = [
+//     {
+//         mode: "production",
+//         entry: "./src/app-index.js",
+//         output: {
+//             path: path.resolve(__dirname, "public"),
+//             filename: "app.js"
+//         }
+//     },
+//     {
+//         mode: "production",
+//         entry: "./src/app.js",
+//         output: {
+//             path: path.resolve(__dirname, "public/app"),
+//             filename: "app.js"
+//         }
+//     }
+// ]
