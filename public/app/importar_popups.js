34d388031c1c00d83b117e54df2479b261d1c02e
André Moura <contato@andremourasantos.com>
2023-04-07 10:53:05 -0300

    script de importar popups transferido para o app.js e responde pelo método POPUPS.IMPORTAR

--- public/app/importar_popups.js
+++
@@ -1,7 +0,0 @@
-async function importarPopup(identificador_do_popup){
-    const popups = await fetch('/app/pedacos/popups.html').then(res => {return res.text()})
-    const parser = new DOMParser();
-    const HTML = parser.parseFromString(popups, 'text/html');
-    const POPUP = HTML.querySelector(`[data-popup="${identificador_do_popup}"]`);
-    document.body.appendChild(POPUP)
-};
