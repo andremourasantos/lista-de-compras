2359257a4e7ae30c9b59a57faa596e0ebe58e75f
André Moura Santos <92397834+andremourasantos@users.noreply.github.com>
2023-10-27 22:46:29 +0000

    alpha 1.11 - Adicionada lógica para correta exibição das informações do componente ListCompanion.vue.

--- src/components/app/ListCompanion.vue
+++ src/components/app/ListCompanion.vue
@@ -2,7 +2,7 @@
   <div>
     <section>
       <ph-hand-coins :size="20" color="#707070" weight="light" />
-      <span>Total: R$ {{ styledTotalCost }}</span>
+      <span>Total: R$ {{ totalCost }}</span>
     </section>
     <section>
       <ph-circles-four :size="20" color="#707070" weight="light" />
@@ -20,13 +20,21 @@ import userInfo from '@/store/user-info';
 export default defineComponent({
   setup () {
     const userData = ref<userInfo>(userInfo);
-    const totalCost = ref<number>(0.00);
+    const totalCost = ref<string>('0');
     const totalItems = ref<number>(0);
-    const styledTotalCost:string = Intl.NumberFormat().format(totalCost.value);
     const styledItemsString:string = totalItems.value === 1 ? 'item' : 'itens';
 
+    watch(userData.value, (newValue) => {
+      if(newValue.userList === null){return}
+      totalItems.value = newValue.userList.length;
+
+      const priceList = newValue.userList.map(obj => {return obj.tags.price}).reduce((acc, price) => acc + price, 0);
+      const styledNumber = new Intl.NumberFormat('pt-BR', {minimumFractionDigits: 2}).format(priceList)
+      totalCost.value = styledNumber;
+    })
+
     return {
-      styledTotalCost,
+      totalCost,
       totalItems,
       styledItemsString
     }
