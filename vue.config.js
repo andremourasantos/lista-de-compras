679ed9d84f12b58d70ce6887fbe2c7bf8478938c
André Moura Santos <92397834+andremourasantos@users.noreply.github.com>
2023-10-31 21:08:57 +0000

    beta 1.2 - Adicionada descrição SEO para o site. - Adicionada foto de capa para o site. - Ajustes nas definições do manifestOptions para incorporar um maskable icon e também capturas de tela para serem exibidas na instalação do PWA.

--- vue.config.js
+++ vue.config.js
@@ -5,47 +5,88 @@ module.exports = defineConfig({
     port: 5500
   },
   pwa: {
+    assetsVersion: 'b-1.2',
     manifestOptions: {
       "name": "Lista de compras",
-      "description": "Sua nova lista de compras para o mercado.",
       "short_name": "Lista de compras",
+      "description": "Sua nova lista de compras vai ajudar você a se manter organizado e a economizar durante as idas ao mercado.",
+      "categories": ["finance", "food", "utilities"],
       "start_url": '/?source=pwa',
+      "background_color": "#FFFFFF",
+      "display": 'standalone',
+      "scope": "/",
+      "theme_color": "#FFFFFF",
       "icons": [
         {
-          "src": "./img/favicons/android-chrome-192x192.png",
+          "src": "./img/icons/android-chrome-192x192.png",
           "sizes": "192x192",
           "type": "image/png"
         },
         {
-          "src": "./img/favicons/android-chrome-256x256.png",
+          "src": "./img/icons/android-chrome-256x256.png",
           "sizes": "256x256",
           "type": "image/png"
         },
         {
-          "src": "./img/favicons/apple-touch-icon.png",
+          "src": "./img/icons/apple-touch-icon.png",
           "sizes": "180x180",
           "type": "image/png"
         },
         {
-          "src": "./img/favicons/favicon-16x16.png",
+          "src": "./img/icons/favicon-16x16.png",
           "sizes": "16x16",
           "type": "image/png"
         },
         {
-          "src": "./img/favicons/favicon-32x32.png",
+          "src": "./img/icons/favicon-32x32.png",
           "sizes": "32x32",
           "type": "image/png"
         },
         {
-          "src": "./img/favicons/mstile-150x150.png",
-          "sizes": "150x150",
-          "type": "image/png"
+          "src": "./img/icons/maskable-icon.png",
+          "sizes": "300x300",
+          "type": "image/png",
+          "purpose": "maskable"
         }
       ],
-      "background_color": "#FFFFFF",
-      "display": 'standalone',
-      "scope": "/",
-      "theme_color": "#FFFFFF"
+      "screenshots": [
+        {
+          "src": "img/promotional/0-lista-vazia.png",
+          "sizes": "1079x2110",
+          "form_factor": "narrow",
+          "label": "A sua nova lista de compras está esperando por você!"
+        },
+        {
+          "src": "img/promotional/1-lista-preenchida.png",
+          "sizes": "1079x2110",
+          "form_factor": "narrow",
+          "label": "Adicione todos os items que precisar, com os detalhes que precisar."
+        },
+        {
+          "src": "img/promotional/2-editando-item.png",
+          "sizes": "1079x2110",
+          "form_factor": "narrow",
+          "label": "Mudou de ideia? Sem problemas, basta clicar no item para editá-lo."
+        },
+        {
+          "src": "img/promotional/0-wide-lista-vazia.png",
+          "sizes": "2276x1422",
+          "form_factor": "wide",
+          "label": "A sua nova lista de compras está esperando por você!"
+        },
+        {
+          "src": "img/promotional/1-wide-lista-preenchida.png",
+          "sizes": "2276x1422",
+          "form_factor": "wide",
+          "label": "Adicione todos os items que precisar, com os detalhes que precisar."
+        },
+        {
+          "src": "img/promotional/2-wide-editando-item.png",
+          "sizes": "2276x1422",
+          "form_factor": "wide",
+          "label": "Mudou de ideia? Sem problemas, basta clicar no item para editá-lo."
+        }
+      ]
     },
     workboxPluginMode: 'GenerateSW'
   }
